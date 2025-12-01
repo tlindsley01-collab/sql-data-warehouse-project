5116b317c867a2d50475fc097668e0cde8ffa366
BoxOfGridSquares <tlindsley01@gmail.com>
2025-12-01 12:32:59 -0500

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,100 @@
+/*
+==============================================================================
+DDL Script:  Create Gold Views
+==============================================================================
+Script Purpose:
+	This script creates views for the Gold Layer in the data warehouse.
+	The Gold layer represents the final dimension and fact tables (Star Schema)
 
+	Each view performs transformations and combines data from the Silver layer
+	to produce a clean, enriched, and business-ready dataset
+
+Usage:
+	- These views can be queried directly for analytics and reporting
+
+==============================================================================
+*/
+
+--============================================================================
+--Create Dimension:  gold.dim_customer
+--============================================================================
+IF OBJECT_ID('gold.dim_customer','V') IS NOT NULL
+	DROP VIEW gold.dim_customer;
+GO
+
+	CREATE VIEW gold.dim_customer AS
+	SELECT
+		ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, --Assigns surrogate key (primary key to all records)
+		ci.cst_id AS customer_id,
+		ci.cst_key AS customer_number,
+		ci.cst_firstname AS first_name,
+		ci.cst_lastname AS last_name,
+		la.cntry AS country,
+		ci.cst_marital_status AS marital_status,
+		CASE WHEN ci.cst_gndr != 'n/a'  THEN ci.cst_gndr -- CRM is master for gender info
+			ELSE COALESCE(ca.gen, 'n/a')
+		END AS gender,
+		ca.bdate AS birthdate,
+		ci.cst_create_date as create_date
+		
+		
+	FROM silver.crm_cust_info ci -- silver.crm_cust_info serves as the master table to which the other customer tables will be joined
+								 -- "ci" is the alias being assigned to this table.  To enable use in the query
+	LEFT JOIN silver.erp_cust_az12 ca -- "ca" is the alias being assigned to this table.  To enable use in the query
+	ON		  ci.cst_key = ca.cid -- joining on these two fields in alignment with the data model and data clean-up work that was done in the Silver layer
+	LEFT JOIN silver.erp_loc_a101 la -- "la" is the alias being assigned to this table.  To enable use in the query
+	ON	      ci.cst_key = la.cid; -- joining on these two fields in alignment with the data model and data clean-up work that was done in the Silver layer
+GO
+
+--============================================================================
+--Create Dimension:  gold.dim_products
+--============================================================================
+IF OBJECT_ID('gold.dim_products','V') IS NOT NULL
+	DROP VIEW gold.dim_products
+GO
+
+	CREATE VIEW gold.dim_products AS
+
+	SELECT
+		ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, --Create and assign primary key
+		pn.prd_id AS product_id,
+		pn.prd_key AS product_number,
+		pn.prd_nm AS product_name,
+		pn.cat_id AS category_id,
+		pc.cat AS category,
+		pc.subcat AS subcategory,
+		pc.maintenance,	
+		pn.prd_cost AS cost,
+		pn.prd_line as product_line,
+		pn.prd_start_dt AS start_date
+	FROM silver.crm_prd_info pn -- silver.crm_prd_info serves as the master table to which the ERP product table will be joined
+								-- "pn" is the alias being assigned to this table.  To enable use in the query
+	LEFT JOIN silver.erp_px_cat_g1v2 pc -- "pc" is the alias being assigned to this table.  To enable use in the query
+	ON pn.cat_id = pc.id
+	WHERE prd_end_dt IS NULL; -- Filter out all historical data
+GO
+--============================================================================
+--Create Fact:  gold.fact_sales
+--============================================================================
+IF OBJECT_ID('gold.fact_sales','V') IS NOT NULL
+	DROP VIEW gold.fact_sales
+GO
+
+	--Create Sales Facts Table Joining Customer and Product Dimension to Sales Data
+	CREATE VIEW gold.fact_sales AS
+	SELECT
+		sd.sls_ord_num AS order_number,
+		pr.product_key,
+		cu.customer_key,
+		sd.sls_order_dt AS order_date,
+		sd.sls_ship_dt AS shipping_date,
+		sd.sls_due_dt AS due_date,
+		sd.sls_sales AS sales_amount,
+		sd.sls_quantity AS quantity,
+		sd.sls_price AS price
+	FROM silver.crm_sales_details sd
+	LEFT JOIN gold.dim_products pr
+	ON sd.sls_prd_key = pr.product_number
+	LEFT JOIN gold.dim_customer cu
+	ON sd.sls_cust_id = cu.customer_id;
+GO
